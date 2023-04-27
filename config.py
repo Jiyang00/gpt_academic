74f0458aea6ccab3e0b5ec3af21fd05ced5fcca5
wangjixiang@yarward.com <wangjixiang@yarward.com>
2023-04-27 13:08:56 +0800

    fix API key

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # [step 1]>> 例如： API_KEY = "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" （此key无效）
-API_KEY = "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"    # 可同时填写多个API-KEY，用英文逗号分割，例如API_KEY = "sk-openaikey1,sk-openaikey2,fkxxxx-api2dkey1,fkxxxx-api2dkey2"
+API_KEY = "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"    # 可同时填写多个API-KEY，用英文逗号分割，例如API_KEY = "sk-openaikey1,sk-openaikey2,fkxxxx-api2dkey1,fkxxxx-api2dkey2"
 
 # [step 2]>> 改为True应用代理，如果直接在海外服务器部署，此处不修改
 USE_PROXY = True
